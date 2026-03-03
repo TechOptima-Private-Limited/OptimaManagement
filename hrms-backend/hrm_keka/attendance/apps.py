293347b659c8938ae183a672180ed8c49bd2f48c
Jayavardhan1407 <pakanati.jayavardhan@gmail.com>
2026-03-03 11:43:10 +0530

    Revert "Initial commit - frontend and backend setup"
    This reverts commit 778535361ac87b64853f14cc083e50614b4a5857.

--- hrms-backend/hrm_keka/attendance/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AttendanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'attendance'
-
-    def ready(self):
-        import attendance.signals  # ✅ This line is required to trigger the signal registration
--- hrms-backend/hrm_keka/onboarding1/apps.py
+++
@@ -1,9 +0,0 @@
-# onboarding/apps.py
-from django.apps import AppConfig
-
-class OnboardingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'onboarding'
-    
-    # def ready(self):
-    #     import onboarding.signals  # Import signals when app is ready
