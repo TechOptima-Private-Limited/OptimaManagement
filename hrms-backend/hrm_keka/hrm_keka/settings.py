3180dfe9e62788b1f436e3b6620cd10dd320f8d4
Sai13478 <saipramodvedantam@gmail.com>
2026-03-04 15:24:52 +0530

    added docker

--- hrms-backend/hrm_keka/hrm_keka/settings.py
+++ hrms-backend/hrm_keka/hrm_keka/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'resource_management',
     'dal',
     'dal_select2',
-    # 'django_ckeditor_5',
+    'django_ckeditor_5',
     'assets',
     'webpush',
     'captcha',
@@ -86,6 +86,7 @@ LOGGING = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
